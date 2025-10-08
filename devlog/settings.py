e1e1b101541acf22c20df31dcd870d63fc750af2
Olegs <olegs.krivko@gmail.com>
2025-10-09 00:15:03 +0300

    fix url

--- devlog/settings.py
+++ devlog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -92,12 +93,28 @@ WSGI_APPLICATION = 'devlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv("DATABASES_ENGINE"),
+#         'NAME': os.getenv("DATABASES_NAME"),
+#         'USER': os.getenv("DATABASES_USER"),
+#         'PASSWORD': os.getenv("DATABASES_PASSWORD"),
+#         'HOST': os.getenv("DATABASES_HOST"),
+#         'PORT': os.getenv("DATABASES_PORT"),
+#         # 'OPTIONS': {
+#         #     'sslmode': 'require',                       
+#         # },
+#     }
+# }
 
 
 # Password validation
